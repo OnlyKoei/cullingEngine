d74808091673391be84cf7a0939a94d0bd24cc24
onlykoei <215194780@qq.com>
2022-09-01 19:29:59 +0800

    第三次
    内置材质剔除，编辑器可能会引用报错

--- engine/cocos/2d/renderer/render-data.ts
+++ engine/cocos/2d/renderer/render-data.ts
@@ -826,4 +826,4 @@ export class MeshRenderData extends BaseRenderData {
     }
 }
 
-const _meshDataPool: RecyclePool<MeshRenderData> = new RecyclePool(() => new MeshRenderData(), 32);
+// const _meshDataPool: RecyclePool<MeshRenderData> = new RecyclePool(() => new MeshRenderData(), 32);
